25e05646df87fa3f2eae8080fc67874401da305f
MalikHashirShakeel <cyberpunk19825@gmail.com>
2025-03-03 17:18:30 +0500

    Improved announcements and assignments vewing and also updated messages

--- classroom_web_app/about_us/admin.py
+++ classroom_web_app/about_us/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'short_message', 'created_at')  
+    search_fields = ('name', 'email', 'message')  
+    list_filter = ('created_at',)  
+    ordering = ('-created_at',)  
+
+    def short_message(self, obj):
+        return obj.message[:50] + ("..." if len(obj.message) > 50 else "")
+    short_message.short_description = "Message"
+
+admin.site.register(Message, MessageAdmin)
